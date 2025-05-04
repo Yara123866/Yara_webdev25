dfd359a39eb29b34151beedc2b90c0a0da5f48a6
Yara136 <ysg6725@nyu.edu>
2025-05-04 16:02:53 -0400

    Adding Final_Project folder for submission

---
+++ Final_Project/README.md
@@ -0,0 +1,4 @@
+Link To Site: file:///C:/Users/yaras/Desktop/WebDevClassFiles/Yara_webdev25/Final_Project/index.html
+
+Link To Documentation/Presentation: https://docs.google.com/presentation/d/1z3e6o-7eevgVYNBzxi-b53eoj8GYiOkcamSmuFSMo4I/edit?slide=id.g35493faf6c5_1_3&pli=1#slide=id.g35493faf6c5_1_3
+
---
+++ Final_Project/main.js
@@ -0,0 +1,43 @@
+function createColour() {
+    var hexcodes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    var hex = ['#'];
+    for (let i = 0; i < 6; i++) {
+        var num = Math.floor(Math.random() * 11)
+        hex += hexcodes[num]
+    }
+    let hexcode = hex.toString();
+    return hexcode
+}
+
+function changePalette() {
+    let codes = []
+    let firstDiv = document.getElementById('one');
+    let secondDiv = document.getElementById('two');
+    let thirdDiv = document.getElementById('three');
+    let fourthDiv = document.getElementById('four');
+    let fifthDiv = document.getElementById('five');
+    let divs = [firstDiv, secondDiv, thirdDiv, fourthDiv, fifthDiv]
+
+    for (let i=0; i<5; i++) {
+        let hexcode = createColour();
+        codes.push(hexcode);
+        document.getElementById(`${divs[i].id}`).style.backgroundColor = codes[i];
+        document.getElementById(`${divs[i].id}Head`).textContent = `${codes[i]}`
+    }
+}
+
+document.querySelectorAll("h1").forEach(head => {
+    head.addEventListener("click", function () {
+        let colorText = this.innerText;
+    navigator.clipboard.writeText(colorText).then(()=> {
+        alert("Copied: " + colorText);
+    
+    });
+});
+});
+
+
+
+
+
+
